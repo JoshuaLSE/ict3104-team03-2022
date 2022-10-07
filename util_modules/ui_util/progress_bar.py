f6aeecec99fa13d75537243f7ade7cd8c72c31ee
Tan Hui Yang <44188809+Teruhii@users.noreply.github.com>
2022-10-07 22:49:54 +0700

    Complete progress_bar UI T03-25, T03-28 (#24)
    - Created class for progress_bar
    - Modularised progress_bar and moved under ui_util
    - Moved json_util under util_modules folder
    - Completed progress_bar UI utility for common use between tracking/updating via epoch count from training/testing

---
+++ util_modules/ui_util/progress_bar.py
@@ -0,0 +1,22 @@
+from tqdm.notebook import tqdm
+class progress_bar:
+    def __init__(self, total_epoch):
+        self.total_epoch = total_epoch
+        self.current_epoch = 0
+        self.display = False
+
+    def display_bar(self):
+        print("x = 1+1")
+        print(self.total_epoch)
+        self.bar = tqdm(total=self.total_epoch)
+        self.display = True
+    def update_bar(self):
+        if self.display == False:
+            print("bar is not displayed yet!")
+        else:
+            self.bar.update(1)
+            self.current_epoch += 1
+            if self.current_epoch >= self.total_epoch:
+                self.bar.close()            
+    def clean_bar(self):
+        self.bar.close()
